aa8fe3e562b3e359fd5e54c30658e434d223defc
ben9333 <123626324+ben9333@users.noreply.github.com>
2023-03-15 12:04:49 +0100

    1

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+
+
+<title>Graphe Chart JS</title>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+
+</head>
+
+
+
+<body>
+	<div>
+		<canvas id="myChart"></canvas>
+    </div>
+
+
+	<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+
+</body>
+</html>
+  
